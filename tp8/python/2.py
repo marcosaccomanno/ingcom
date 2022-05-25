7623e19b494a262eaf852af2d5a6ef48a3a713dd
marco <marcosaccomanno@protonmail.com>
2022-05-25 12:10:05 -0300

    Agregado el archivo 2.py

--- tp8/python/2.py
+++ tp8/python/2.py
@@ -3,9 +3,21 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+def intervalo(numero):
+    if numero >= -1 and numero < 0:
+        resultado = "pertenece al intervalo [-1; 0)"
+    elif numero > 0 and numero <= 1:
+        resultado = "pertenece al intervalo (0; 1]"
+    else:
+        resultado = "no pertenece a ninguno de los intervalos"
+    return resultado
+
 def principal():
     '''Funcion principal'''
-   pass
+    x = float(input("Ingrese un numero: "))
+    que_intervalo = intervalo(x)
+    print(f"{x} {que_intervalo}")
+
 if __name__ == "__main__":
     principal()
     
